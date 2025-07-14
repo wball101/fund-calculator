e1551041e1444c14e9e81434396d4057752a0987
wball101 <100445625+wball101@users.noreply.github.com>
2025-07-14 12:15:43 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 
 st.set_page_config(page_title="Fund Split Calculator", layout="centered")
 st.title("💰 Fund Split Calculator")
